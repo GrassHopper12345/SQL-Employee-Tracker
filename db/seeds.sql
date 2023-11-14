a32b0db67d623c8df8ab72473ae3d7132b7dabf1
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-11-13 21:01:20 -0600

    update to seeds file with values for department table and roles table

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department(name)
+VALUES
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO role(title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 1)
+('Salesperson', 80000, 2),
+('Lead Engineer', 150000, 3),
+('Software Engineer',120000, 4),
+('Account Manager', 160000, 5),
+('Accountant', 125000, 6),
+('Legal Team Lead', 250000, 7),
+('Lawyer', 190000, 8);
+
+INSERT INTO employee
+
