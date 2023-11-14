df7a4b06ee7e5e882d2a226eadc631512ed34fdd
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-11-14 16:49:34 -0600

    update to seeds file with employee list

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ VALUES
 
 INSERT INTO role(title, salary, department_id)
 VALUES
-('Sales Lead', 100000, 1)
+('Sales Lead', 100000, 1),
 ('Salesperson', 80000, 2),
 ('Lead Engineer', 150000, 3),
 ('Software Engineer',120000, 4),
@@ -17,4 +17,14 @@ VALUES
 ('Lawyer', 190000, 8);
 
 INSERT INTO employee
+(first_name, last_name, role_id, manager)
+VALUES
+(John, Doe, 1, null),
+(Mike, Chan, 2, John Doe),
+(Ashley, Rodriquez, 3, null),
+(Kevin, Tupik, 4, Ashley Rodriguez),
+(Kunal, Singh, 5, null),
+(Malia, Brown, 6, Kunal Singh),
+(Sarah, Lourd, 7, null),
+(Tom, Allen, 8, Sarah Lourd);
 
