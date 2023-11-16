dce62a2adf9866cb69a5ee05023b6d9f266d7722
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-11-16 17:10:45 -0600

    update to index.js file, created prompts for updating role for an employee by choosing the employee the compare and set var

--- index.js
+++ index.js
@@ -126,37 +126,124 @@ var employeeTracker = function () {
                         validate: firstNameAdded => {
                             if (firstNameAdded) {
                                 return true;
-                        }else {
-                            console.log(`Please enter the employee's first name.`);
-                            return false;
+                            } else {
+                                console.log(`Please enter the employee's first name.`);
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        //add employee last name
+                        type: 'input',
+                        name: 'lastName',
+                        message: `Please enter the employee's last name.`,
+                        validate: lastNameAdded => {
+                            if (lastNameAdded) {
+                                return true;
+                            } else {
+                                console.log(`Please enter the employee's last name`);
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        //add an employees role
+                        type: 'list',
+                        name: 'role',
+                        messsage: `Please choose from the list provided the employee's role.`,
+                        choices: () => {
+                            var array = [];
+                            for (var i = 0; i < result.length; i++) {
+                                array.push(result[i].title);
+                            }
+                            var newArray = [...new Set(array)];
+                            return newArray;
+                        }
+                    },
+                    {
+                        //adding manager for added employee
+                        type: 'input',
+                        name: 'manager',
+                        message: `Please add the employee's manager`,
+                        validate: managerAdded => {
+                            if (managerAdded) {
+                                return true;
+                            } else {
+                                console.log(`Please enter the employee's manager`);
+                                return false;
+                            }
                         }
                     }
-                },
-                {
-                    //add employee last name
-                    type: 'input',
-                    name: 'lastName',
-                    message: `Please enter the employee's last name.`,
-                    validate: lastNameAdded => {
-                        if (lastNameAdded) {
-                            return true;
-                        }else {
-                            console.log(`Please enter the employee's last name`);
-                            return false;
+                ]).then((answers) => {
+                    // compare result and store in new variable
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i].title === answers.role) {
+                            var role = result[i];
                         }
                     }
-                },
-                {
-                    //add an employees role
-                    type: 'list',
-                    name: 'role',
-                    messsage: `Please choose from the list provided the employee's role.`
-                },
-                ])
-            })
-        } 
-
-
+                    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`, [answers.firstName, answers.lastName, role.id, answers.manager.id], (err, result) => {
+                        if (err) throw err;
+                        console.log(`Added ${answers.firstName} ${answers.lastName} to database.`);
+                        employeeTracker();
+                    });
+                })
+            });
+        } else if (answers.prompt === 'Update An Employee Role') {
+            // acquire roles and managers from database
+            db.query(`SELECT * FROM employee, role`, (err, result) => {
+                if (err) throw err;
+                inquirer.prompt([
+                    {
+                        //choosing employee to update
+                        type: 'list',
+                        name: 'employee',
+                        message: 'Please select an employee you would like to update',
+                        choices: () => {
+                            var array = [];
+                            for (var i = 0; i < result.length; i++) {
+                                array.push(result[i].last_name);
+                            }
+                            var employeeArray = { ...new Set(array) };
+                            return employeeArray;
+                        }
+                    },
+                    {
+                        //updating the new role in database
+                        type: 'list',
+                        name: 'role',
+                        meesage: 'Please choose from the provided list the role of the new employee.',
+                        choices: () => {
+                            var array = [];
+                            for (var i = 0; i < result.length; i++) {
+                                array.push(result[i].title);
+                            }
+                            var roleAdded = { ...new Set(array) };
+                            return roleAdded;
+                        }
+                    }
+                ]).then((answers) => {
+                    //compare result and store in new variable
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i].last_name === answers.employee) {
+                            var lastName = result[i];
+                        }
+                    }
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i].title === answers.role) {
+                            var role = result[i];
+                        }
+                    }
+                    db.query(`UPDATE employee SET ? WHERE ?`, [{role_id: role}, {last_name: lastName}], (err, result) => {
+                        if(err) throw err;
+                        console.log(`Updated ${answers.employee} role to the database.`);
+                        employeeTracker();
+                    });
+                })
+            });
+        }else if (answers.prompt === 'Log Out') {
+            db.end();
+            console.log('You are now logged out, Good-Bye!');
+        }
     })
 };
 
